0133ef7b639ba1c60bf8564d86a2b233dc4ef5e5
Jose Ricardo Ventrilho Filho <jose.ventrilho@mercadolivre.com>
2023-03-30 10:53:29 -0300

    Adjust exercise number 4

--- 4 - Docker compose/index.js
+++ 4 - Docker compose/index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('My application web'))
+let numberOfRequest = 0
+
+app.get('/', (req, res) => {
+    res.send('My application web')
+
+    numberOfRequest++
+
+    fs.writeFile("tmp/log.txt", `My application web, total de request = ${numberOfRequest}`, (err) => {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log("The file was saved!")
+    })
+})
 
 app.listen(3000)
